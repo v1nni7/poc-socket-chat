a72eb000ac65169e4024bf25fa219b856b2f790c
Vinicius <vinicezar800@hotmail.com>
2023-06-04 12:53:26 -0300

    feat: created a socket emit for send messages to client side

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,34 @@
 import express, { json } from "express";
+import { Server } from "socket.io";
+import http from "http";
 import cors from "cors";
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
 
 app.use(cors());
 app.use(json());
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+io.on("connection", (socket) => {
+  socket.on("message", (msg) => {
+    io.emit("message", msg)
+  });
+});
+
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
