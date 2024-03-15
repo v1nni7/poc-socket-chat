009c86c57411daa1659e81a95ffe9bbe0f131650
Vinicius <vinicezar800@hotmail.com>
2024-03-14 22:39:41 -0300

    build: change scripts to build project on render

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -11,6 +11,25 @@ const io = new Server(server, {
   },
 })
 
+const rooms = {}
+
+// io.on('connection', (socket) => {
+//   socket.on('join-room', (data) => {
+//     socket.join(data.callURL)
+
+//     console.log('Usuário conectando na sala', data)
+//     // Emitir um evento para informar que um usuário se conectou à sala
+//     io.to(data.callURL).emit('user-connected', { name: data.name })
+//   })
+
+//   socket.on('signal', (data) => {
+//     // Transmitir o sinal para o outro peer na sala
+//     socket.to(data.room).emit('signal', { signalData: data.signalData })
+//   })
+// })
+
+export default server
+
 io.on('connection', (socket) => {
   socket.emit('me', socket.id)
 
@@ -30,5 +49,3 @@ io.on('connection', (socket) => {
     io.to(data.to).emit('callAccepted', data.signal)
   })
 })
-
-export default server
